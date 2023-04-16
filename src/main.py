72d20061c5293a009495fce10a67903667394d92
ben-moohrey <benjaminhudsonmurray@gmail.com>
2023-04-16 19:53:34 -0400

    added argument support in main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,34 @@
 """main.py"""
 
-def main():
+import sys
+
+
+
+user = ""
+prompt = ""
+
+def main(args):
     """
     Main function to run complete program
     """
-    print("Hello World")
+    global user
+    global prompt
+    for i in range(1,len(args[1:])+1):
+
+        # User
+        if i == 1:  
+            user = str(args[i])
+            print("User:", user)
+
+        # Prompt
+        if i == 2:
+            prompt = str(args[i])
+            print("Prompt:", prompt)
+
+
+    
 
 
 if __name__ == "__main__":
-    main()
+
+    main(sys.argv)
