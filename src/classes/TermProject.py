ef124c0f2913c0f9d17c2e7434d9e4aa48c7f8b9
ben-moohrey <benjaminhudsonmurray@gmail.com>
2023-04-16 22:17:26 -0400

    Fixed auth

---
+++ src/classes/TermProject.py
@@ -0,0 +1,15 @@
+"""
+TermProject.py
+
+Class contains the logic to scrape the followers from a specific user and return 5
+tweets that are in the style
+"""
+
+
+class TermProject:
+    def __init__(self, user):
+        self.user = user
+
+    def get_top_tweets(self):
+        print("TOP TWEETS")
+
